ef5130738e2a225ddb2a9c33547a8cf8fec7197c
andrew.sales <andjsales@gmail.com>
2024-08-03 22:27:47 -0500

    Update to bind to Heroku's port

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
